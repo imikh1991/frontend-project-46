b790425eb1fbe349de21e6591f52be5a1682af00
imikh1991 <ilya.mikhaylyukov@gmail.com>
2023-08-01 20:55:34 +0700

    add: genDiff-function

--- fs.js
+++ fs.js
@@ -1,15 +1,37 @@
+/* eslint-disable no-restricted-syntax */
 import fs from 'fs';
+import _ from 'lodash';
 
-const { promises: fsp } = fs;
-
-const foo = async (filepath) => {
+export const readFileData = (filePath) => {
   try {
-    const data = await fsp.readFile(filepath, 'utf-8');
-    console.log(`Data read asynchronously:\n${data}`);
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return data;
   } catch (error) {
-    console.log(error);
+    console.log('Error occured', error.message);
+    return null;
   }
 };
 
-foo('src/files/file1.json');
-foo('src/files/file2.json');
+export const getSortedKeys = (data) => {
+  try {
+    const obj = JSON.parse(data);
+    const sortedKeys = _.fromPairs(_.sortBy(Object.entries(obj), ([key]) => key));
+    return sortedKeys;
+  } catch (error) {
+    console.log('Error occured', error.message);
+    return null;
+  }
+};
+// начните с чтения файлов V
+// получите данные из файлов, V
+// распарсите данные в объекты и массивы, V
+// чтобы их можно было сравнивать. V
+// Затем нужно сформировать строку для вывода.
+// параметр ф-ции -> c относительными, так и абсолютными путями V
+// до файлов (полезные функции: path.resolve() и process.cwd()) V
+// Сравниваются данные, а не строки файлов V
+// Две строчки дифа, отвечающие за различия поля, V
+// должны находиться рядом. Причём вначале должна выводиться
+// строка относящаяся к первому файлу, а затем строка относящаяся
+// ко второму файлу (см. пример с timeout)
+// Результатом работы функции genDiff() является строка
--- gendiff.js
+++ gendiff.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+import path from 'path';
 import { Command } from 'commander';
+import { getSortedKeys, readFileData } from './fs.js';
 
 const program = new Command();
 
@@ -12,18 +14,56 @@ program
   .on('--help', () => {
     console.log('');
     console.log('Examples:');
-    console.log('  $ gendiff file1.json file2.json');
+    console.log('  $ gendiff read file1.json file2.json');
     console.log('  $ gendiff -f plain file1.yml file2.yml');
   });
 
-program.command('split')
-  .description('Compares two configuration files and shows a difference.')
-  .argument('<string>', 'string to split')
-  .option('-f, --format <type>', 'format of pizza')
+program.command('read')
+  .description('read two configuration files and shows a difference.')
+  .argument('<filepath1>', 'path to the first file')
+  .argument('<filepath2>', 'path to the second file')
+  .option('-f, --format <type>', 'format')
   .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
+  .action((filepath1, filepath2) => {
+    const absolutePath1 = path.resolve(process.cwd(), filepath1);
+    const absolutePath2 = path.resolve(process.cwd(), filepath2);
+
+    const data1 = readFileData(absolutePath1);
+    const data2 = readFileData(absolutePath2);
+
+    if (data1 && data2) {
+      const sortedKeys1 = Object.keys(getSortedKeys(data1));
+      const sortedKeys2 = Object.keys(getSortedKeys(data2));
+
+      const getResult = (sortKeys1, sortKeys2) => {
+        const result = [];
+        sortKeys1.forEach((key) => {
+          const value1 = getSortedKeys(data1)[key];
+          const value2 = getSortedKeys(data2)[key];
+          if (sortKeys2.includes(key)) {
+            if (value1 !== value2) {
+              result.push(`- ${key}: ${value1}`);
+              result.push(`+ ${key}: ${value2}`);
+            } else {
+              result.push(`  ${key}: ${value1}`);
+            }
+          } else {
+            result.push(`- ${key}: ${value1}`);
+          }
+        });
+
+        sortKeys2.forEach((key) => {
+          const value2 = getSortedKeys(data2)[key];
+          if (!sortKeys1.includes(key)) {
+            result.push(`+ ${key}: ${value2}`);
+          }
+        });
+
+        console.log(`{\n${result.join('\n')}\n}`);
+      };
+
+      getResult(sortedKeys1, sortedKeys2);
+    }
   });
 
 program.parse();
